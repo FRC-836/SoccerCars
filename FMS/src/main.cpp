309cd2f49659fb5348a83edc5cdbb9bdeedc5da6
Cheeto836 <mprussell836@gmail.com>
2018-05-25 17:03:01 -0400

    cmd line options are now being handled

--- FMS/src/main.cpp
+++ FMS/src/main.cpp
@@ -15,6 +15,18 @@ int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
+  QCommandLineParser parser;
+  if (!parseCmd(parser))
+  {
+    parser.showHelp(1);
+  } //end  if (!parseCmd(parser))
+
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: main: verbosity   = " << CmdOptions::verbosity << endl;;
+    cout << "INFO: main: config file = " << CmdOptions::configPath << endl;;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+
   MainMenu mainMenu;
   mainMenu.show();
 
@@ -29,9 +41,8 @@ bool parseCmd(QCommandLineParser& parser)
   parser.setApplicationDescription("Used to create and modify QCP projects");
 
   //debug level option
-  QCommandLineOption debugOption({ "v", "verbosity" }, "Verbosity of the info printed",
-                                 "debugLevel", "2");
-  parser.addOption(debugOption);
+  parser.addOptions({ { {"v", "verbosity"}, "Verbosit of the info displayed", "debugLevel", "2"},
+                      { {"c", "config"}, "Location of the config file", "configPath", ""} });
 
   //--------------------------------------------------
   //parse the arguments into structure
@@ -43,11 +54,19 @@ bool parseCmd(QCommandLineParser& parser)
   int debugLevel = parser.value("verbosity").toInt(&convesionSuccess);
   if (convesionSuccess)
   {
+    debugLevel = (debugLevel < static_cast<int>(CmdOptions::DEBUG_LEVEL::NO_INFO)) ? 
+                  static_cast<int>(CmdOptions::DEBUG_LEVEL::NO_INFO) : debugLevel;
+    debugLevel = (debugLevel > static_cast<int>(CmdOptions::DEBUG_LEVEL::ALL_INFO)) ? 
+                  static_cast<int>(CmdOptions::DEBUG_LEVEL::ALL_INFO) : debugLevel;
+    CmdOptions::verbosity = static_cast<CmdOptions::DEBUG_LEVEL>(debugLevel);
   } //end  if (convesionSuccess)
   else
   {
     return false;
   } //end  else
+
+  //handle config file path processing
+  CmdOptions::configPath = parser.value("config");
 
     //all command args processed sucessfully
   return true;
