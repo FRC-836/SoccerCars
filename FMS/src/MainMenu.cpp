719cb4f2e60e8c98bee3a76c6e5a7cf2be147c95
Cheeto836 <mprussell836@gmail.com>
2018-05-25 18:43:35 -0400

    added output manager to main menu and the printing of button click information

--- FMS/src/MainMenu.cpp
+++ FMS/src/MainMenu.cpp
@@ -1,5 +1,7 @@
 #include "MainMenu.h"
 
+extern OutputManager cout;
+
 //private functions
 void MainMenu::stopMatch(bool hasWinner)
 {
@@ -24,19 +26,43 @@ MainMenu::~MainMenu()
 //public slots
 void MainMenu::btnMatchSettingsClickHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: MainMenu: match settings button clicked" << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
 }
 void MainMenu::btnCarSettingsClickHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: MainMenu: car settings button clicked" << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
 }
 void MainMenu::btnStartStopClickHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: MainMenu: " <<  m_ui->btnStartStop->text() << " button clicked" << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
 }
 void MainMenu::btnDiagnosticsClickHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: MainMenu: diagnostics button clicked" << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
 }
 void MainMenu::tmrMatchTimerTimeoutHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::USER_INFO)
+  {
+    cout << "INFO: MainMenu: Match timer has ended." << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::USER_INFO)
 }
 void MainMenu::tmrSecondTimerTimeoutHandler()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    cout << "INFO: MainMenu: second timer ticked" << endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
 }
