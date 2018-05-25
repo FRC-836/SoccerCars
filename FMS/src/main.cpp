f1fb303688b5152a221b11aae5a63dfbfae849e4
Cheeto836 <mprussell836@gmail.com>
2018-05-25 16:16:58 -0400

    working on adding cmd line args

--- FMS/src/main.cpp
+++ FMS/src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
 #include <qapplication.h>
+#include <qcommandlineparser.h>
 
 #include "MainMenu.h"
+#include "OutputManager.h"
+#include "CmdOptions.h"
+
+OutputManager cout(stdout);
+
+bool parseCmd(QCommandLineParser& parser);
 
 int main(int argc, char** argv)
 {
@@ -13,3 +20,36 @@ int main(int argc, char** argv)
 
   return app.exec();
 }
+bool parseCmd(QCommandLineParser& parser)
+{
+  //--------------------------------------------------
+  //setup the cmd parser
+  //--------------------------------------------------
+  parser.addHelpOption();
+  parser.setApplicationDescription("Used to create and modify QCP projects");
+
+  //debug level option
+  QCommandLineOption debugOption({ "v", "verbosity" }, "Verbosity of the info printed",
+                                 "debugLevel", "2");
+  parser.addOption(debugOption);
+
+  //--------------------------------------------------
+  //parse the arguments into structure
+  //--------------------------------------------------
+  parser.process(QCoreApplication::arguments());
+  bool convesionSuccess;
+
+  //handle debug level processing
+  int debugLevel = parser.value("verbosity").toInt(&convesionSuccess);
+  if (convesionSuccess)
+  {
+  } //end  if (convesionSuccess)
+  else
+  {
+    return false;
+  } //end  else
+
+    //all command args processed sucessfully
+  return true;
+}  //end bool parseCmd(QCommandLineParser& parser)
+
