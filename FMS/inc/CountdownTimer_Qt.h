040385134f6ebec691a6aa6a5ab3f8688db58b3d
Cheeto836 <mprussell836@gmail.com>
2018-06-04 19:36:48 -0400

    created class to inherit QLCDNumber and act as a countdown timer

---
+++ FMS/inc/CountdownTimer_Qt.h
@@ -0,0 +1,19 @@
+#ifndef SOCCER_CARS_FMS_COUNTDOWN_TIMER_QT_H
+#define SOCCER_CARS_FMS_COUNTDOWN_TIMER_QT_H
+
+#include <qlcdnumber.h>
+
+class CountdownTimer_Qt : public QLCDNumber
+{
+  Q_OBJECT
+
+private:
+
+public:
+
+signals:
+
+public slots:
+};
+
+#endif
