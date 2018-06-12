a3f96c5bbfe0416515ef672ddd170570b89a94fa
Cheeto836 <mprussell836@gmail.com>
2018-06-11 21:01:00 -0400

    set default match length back to proper value

--- FMS/src/MatchOptions.cpp
+++ FMS/src/MatchOptions.cpp
@@ -1,7 +1,7 @@
 #include "MatchOptions.h"
 
 //TODO: switch back to 150, temporarily 10 for ease of testing quickly
-unsigned int MatchOptions::m_seconds = 10; //default length of a match
+unsigned int MatchOptions::m_seconds = 150; //default length of a match
 unsigned int MatchOptions::m_scoreLimit = 15; //default score needed to win a match
 unsigned int MatchOptions::m_numberOfTeams = 2; //default number of teams
 unsigned int MatchOptions::m_carsPerTeam = 3; //default number of cars on each team
