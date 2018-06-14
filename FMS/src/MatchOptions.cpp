b7c917128a81b8456a299a30d9bff1e8b16695e6
Cheeto836 <mprussell836@gmail.com>
2018-06-14 19:20:23 -0400

    removed definitions for functions that no longer exist

--- FMS/src/MatchOptions.cpp
+++ FMS/src/MatchOptions.cpp
@@ -5,21 +5,3 @@ unsigned int MatchOptions::m_seconds = 150; //default length of a match
 unsigned int MatchOptions::m_scoreLimit = 15; //default score needed to win a match
 unsigned int MatchOptions::m_numberOfTeams = 2; //default number of teams
 unsigned int MatchOptions::m_carsPerTeam = 3; //default number of cars on each team
-
-//operators
-bool operator==(const MatchOptions& lhs, const MatchOptions& rhs)
-{
-  if (lhs.m_scoreLimit != rhs.m_scoreLimit)
-  {
-    return false;
-  } //end  if (lhs.m_scoreLimit != rhs.m_scoreLimit)
-  if (lhs.m_seconds != rhs.m_seconds)
-  {
-    return false;
-  } //end  if (lhs.m_seconds != rhs.m_seconds)
-  return true;
-}
-bool operator!=(const MatchOptions& lhs, const MatchOptions& rhs)
-{
-  return !(lhs == rhs);
-}
