88b255f8a1fd84cf6d01fea4f70c166fdbe4b704
Cheeto836 <mprussell836@gmail.com>
2018-06-18 19:32:34 -0400

    added file for containing type alliases

---
+++ FMS/inc/UsefulTypes.h
@@ -0,0 +1,12 @@
+#ifndef SOCCER_CARS_FMS_USEFUL_TYPES_H
+#define SOCCER_CARS_FMS_USEFUL_TYPES_H
+
+#include <qvector.h>
+
+#include "CarOptions.h"
+
+//types aliasses
+using Team_t = QVector<CarOptions>;
+using TeamList_t = QVector<Team_t>;
+
+#endif
